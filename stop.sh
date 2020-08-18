338e111e02b864b8da4ce2c1583c5fbf3783c22a
Vincenzo Marafioti <>
2020-08-18 23:47:43 +0200

    improved link creation

--- stop.sh
+++ stop.sh
@@ -1,1 +1,2 @@
 docker-compose -f env/$1/docker-compose.yml stop
+docker-compose -f env/$1/docker-compose.yml ps
--- up.sh
+++ up.sh
@@ -1,2 +1,3 @@
 docker-compose -f env/$1/docker-compose.yml build
 docker-compose -f env/$1/docker-compose.yml up -d
+docker-compose -f env/$1/docker-compose.yml ps
