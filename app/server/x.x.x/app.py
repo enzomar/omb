c295ac647f67bb856d6c28ddf400883cf12c4345
Vincenzo Marafioti <>
2020-08-19 23:55:54 +0200

    mongo test

--- app/server/x.x.x/app.py
+++ app/server/x.x.x/app.py
@@ -2,27 +2,38 @@ from flask import Flask
 
 from pymongo import MongoClient
 
+import sys, os
+
 app = Flask(__name__)
 
 if app.config["ENV"] == "prd":
-    app.config.from_object("config.PRDConfig")
+	app.config.from_object("config.PRDConfig")
 elif app.config["ENV"] == "uat":
 	app.config.from_object("config.UATConfig")
 
 else:
-    app.config.from_object("config.LOCALConfig")
+	app.config.from_object("config.LOCALConfig")
+
 
 
 def dummy_connect():
-	client = MongoClient(app.config["DB_HOST"], 27017)
+	client = MongoClient(host = app.config["DB_HOST"]+":27017", 
+		username = os.getenv('MONGO_INITDB_ROOT_USERNAME'),
+		password = os.getenv('MONGO_INITDB_ROOT_PASSWORD'))
 	return client
 
 
 
 @app.route('/')
 def hello():
-	m = help("modules")
-    return "{0}: {1} - {2}".format(app.config["ENV"], dummy_connect(), m)
+	m = sys.modules.keys()
+	client = dummy_connect()
+	html  = "<p>{0}</p>".format(app.config["ENV"])
+	html  = "<p>{0}</p>".format(os.getcwd())
+	html += "<p>{0}</p>".format(client.server_info())
+	html += "<p>{0}</p>".format(m)
+
+	return html
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
