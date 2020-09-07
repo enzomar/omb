845f42b9f5d930eda1e8602911e86567bd902928
vincenzo <enzo.mar@gmail.com>
2020-09-07 11:37:18 +0200

    docker compose ovverride

--- app/server/x.x.x/config.py
+++ app/server/x.x.x/config.py
@@ -3,19 +3,18 @@ class Config(object):
     TESTING = False
     MYSQL_DATABASE_USER = 'webserver'
     MYSQL_DATABASE_PASSWORD = 'webserver'
-    MYSQL_DATABASE_DB = 'easycontainer'    
+    MYSQL_DATABASE_DB = 'easycontainer' 
+    MYSQL_DATABASE_HOST = "mysql"   
     
 
-class PRDConfig(Config):
-    MYSQL_DATABASE_HOST = "10.5.0.2"
+class PRDConfig(Config):    
     SESSION_COOKIE_SECURE = True
 
 class LOCALConfig(Config):
     DEBUG = True
-    MYSQL_DATABASE_HOST = "10.2.0.2"
+    
     SESSION_COOKIE_SECURE = False
 
 class UATConfig(Config):
-    TESTING = True
-    MYSQL_DATABASE_HOST = "10.1.0.2"
+    TESTING = True    
     SESSION_COOKIE_SECURE = False
