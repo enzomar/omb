f407288a26ab77dfa898ed171d72e2067f794194
Vincenzo Marafioti <>
2020-09-09 20:54:13 +0200

    improved sanity

--- src/server/10.0.0/app.py
+++ src/server/10.0.0/app.py
@@ -21,23 +21,33 @@ else:
 	app.config.from_object(config.LOCALConfig)
 
 
+
+
 def dummy_connect():
+	from sqlalchemy import create_engine
+	user = app.config['MYSQL_DATABASE_USER']
+	pwd = app.config['MYSQL_DATABASE_PASSWORD']
+	host = app.config['MYSQL_DATABASE_HOST']
+	db_name = app.config['MYSQL_DATABASE_DB']
+	connection_str = 'mysql+pymysql://{0}:{1}@{2}:3306/{3}'.format(user,pwd,host,db_name)
+	return create_engine(connection_str)		
+
+
+def get_tables():
 	try:
-		from sqlalchemy import create_engine
-		user = app.config['MYSQL_DATABASE_USER']
-		pwd = app.config['MYSQL_DATABASE_PASSWORD']
-		host = app.config['MYSQL_DATABASE_HOST']
-		db_name = app.config['MYSQL_DATABASE_DB']
-		connection_str = 'mysql+pymysql://{0}:{1}@{2}:3306/{3}'.format(user,pwd,host,db_name)
-		return create_engine(connection_str).table_names()		
+		return dummy_connect().table_names()		
 	except:
 		return 'Something went bad'
 
-	
 
 @app.route('/')
-def hello():
-	tables =  dummy_connect()
+def api():
+	return "So faf so good..."
+
+
+@app.route('/health')
+def health():
+	tables =  get_tables()
 	m = sys.modules.keys()	
 	html = '<html><body>'
 	html += '<p>Hello from 10.0.0</p>'
@@ -46,9 +56,13 @@ def hello():
 	html += "<p>MYSQL tables: {0}</p>".format(tables)
 	html += "<p>MODULES: {0}</p>".format(m)
 	html += '</body></html>'
-
-
 	return html
+
+
+@app.route('/db')
+def health_db():
+	dummy_connect()
+	return "So faf so good.. DB"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=5000)
